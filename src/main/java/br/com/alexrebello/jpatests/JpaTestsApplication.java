151b8a1294484f99aabb7ca8244482729909738c
lexrebel <lexrebello@gmail.com>
2020-08-15 17:53:57 -0300

    initial commit

---
+++ src/main/java/br/com/alexrebello/jpatests/JpaTestsApplication.java
@@ -0,0 +1,13 @@
+package br.com.alexrebello.jpatests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaTestsApplication.class, args);
+    }
+
+}
